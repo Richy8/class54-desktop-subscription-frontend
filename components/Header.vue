fac9af84fcf2f39536fbadf0a25527f6b3d95ac2
odutolaabisoye <odutolaabisoye@gmail>
2021-05-11 03:41:20 +0100

    Add Slide in Widget

--- components/Header.vue
+++ components/Header.vue
@@ -1,23 +1,27 @@
 <template>
-        <div class="navigation">
-        <div class="inner-container inner--nav">
-            <div class="logo">
-                <nuxt-link to="/">
-                    <img src="/Logo_small.svg" alt="">
-                </nuxt-link>
-                <img src="/Hamburger.svg" alt="" id="hammobileicon">    
-                <img src="/Close.svg" alt="" id="closeicon">                        
-            </div>
-            <div id="menunav" class="menu">
-                <ul>
-                    <li>
-                        <nuxt-link to="/download">Download App</nuxt-link>
-                    </li>
-                    <li>
-                        <nuxt-link to="/activate">Activate App</nuxt-link>
-                    </li>
-                </ul>
-            </div>
-        </div>
+  <div class="navigation">
+    <div class="inner-container inner--nav">
+      <div class="logo">
+        <nuxt-link to="/activate">
+          <img src="/Logo_small.svg" alt="">
+        </nuxt-link>
+        <img id="hammobileicon" src="/Hamburger.svg" alt="">
+        <img id="closeicon" src="/Close.svg" alt="">
+      </div>
+      <div id="menunav" class="menu">
+        <ul>
+          <li>
+            <nuxt-link to="/download">
+              Download App
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/activate">
+              Activate App
+            </nuxt-link>
+          </li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
