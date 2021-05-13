bd40711dfc8d325511600c30be9a6a47f59b9055
odutolaabisoye <odutolaabisoye@gmail>
2021-05-13 10:23:47 +0100

    Email Validate

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
               <path fill-rule="evenodd" clip-rule="evenodd" d="M8.00033 1.99996C4.68662 1.99996 2.00033 4.68625 2.00033 7.99996C2.00033 11.3137 4.68662 14 8.00033 14C11.314 14 14.0003 11.3137 14.0003 7.99996C14.0003 4.68625 11.314 1.99996 8.00033 1.99996ZM0.666992 7.99996C0.666992 3.94987 3.95024 0.666626 8.00033 0.666626C12.0504 0.666626 15.3337 3.94987 15.3337 7.99996C15.3337 12.05 12.0504 15.3333 8.00033 15.3333C3.95024 15.3333 0.666992 12.05 0.666992 7.99996ZM8.00033 7.33329C8.36852 7.33329 8.66699 7.63177 8.66699 7.99996V10.6666C8.66699 11.0348 8.36852 11.3333 8.00033 11.3333C7.63214 11.3333 7.33366 11.0348 7.33366 10.6666V7.99996C7.33366 7.63177 7.63214 7.33329 8.00033 7.33329ZM8.00033 4.66663C7.63214 4.66663 7.33366 4.9651 7.33366 5.33329C7.33366 5.70148 7.63214 5.99996 8.00033 5.99996H8.00699C8.37518 5.99996 8.67366 5.70148 8.67366 5.33329C8.67366 4.9651 8.37518 4.66663 8.00699 4.66663H8.00033Z" fill="white" />
             </svg>
           </div>
-          <p><strong>Email not submitted:</strong> Kindly check and try again.</p>
+          <p><strong>{{ messagestrong }} </strong>{{ message }}</p>
         </div>
         <div class="right" @click="closealert">
           <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -77,7 +77,7 @@
           </svg>
         </span>
         <div class="mailicon">
-          <img src="/images/mailIcon.png" alt="">
+          <img src="/mailIcon.png" alt="">
         </div>
         <p class="modal--head">
           Thank you!
@@ -103,7 +103,9 @@ export default {
       modal: false,
       errors: [],
       notify: false,
-      notifyClassLeave: false
+      notifyClassLeave: false,
+      message: '',
+      messagestrong: ''
     }
   },
   methods: {
@@ -112,6 +114,8 @@ export default {
         this.sendEmail()
       } else {
         this.notify = true
+        this.messagestrong = 'Invalid Email:'
+        this.message = 'Kindly check and try again.'
         setTimeout(() => {
           this.notifyClassLeave = true
         }, 5000)
@@ -139,15 +143,22 @@ export default {
         await this.$axios.post(`https://class54-backend.herokuapp.com/admin/notify/create?.${this.email}`,
           formData
         ).then((response) => {
-          console.log(response.data)
+          console.log(response)
+          // if ()
           this.modal = true
         })
       } catch (e) {
-        this.snackbar = {
-          message: 'There was an error',
-          color: 'error',
-          show: true
-        }
+        console.log('Your email has been taken')
+        this.notify = true
+        this.messagestrong = 'Email has been taken:'
+        this.message = 'Try with another email'
+        setTimeout(() => {
+          this.notifyClassLeave = true
+        }, 5000)
+        setTimeout(() => {
+          this.notify = false
+          this.notifyClassLeave = false
+        }, 6000)
       }
     }
   }
